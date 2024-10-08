56aa3dceb6c1c6bdd944692cfea4fcaa682b1183
Ruqia Razzak <razzakruqia@gmail.com>
2024-10-08 15:45:26 +0500

    Add files via upload

---
+++ JS chp#!/apps.js/apps.js
@@ -0,0 +1,29 @@
+// // 1. Write a script to greet your website visitor using JS alert box.
+// alert("welcome to the javaScript");
+// document.write("welcome to the javaScript" ,"<br>");
+
+
+
+// //2. Write a script to display following message on your web page:
+// alert("Error! plz enter the vaild password");
+// document.write("Error! plz enter the vaild password." ,"<br>");
+
+
+// //3. Write a script to display following message on your web page: 
+// alert("Welcome to jS land...\n Happy Coding!");
+// document.write("Welcome to jS land... Happy Coding!" ,"<br>");
+
+// //4. Write a script to display following messages in sequence:
+// // First alert
+// alert("Welcome to JS Land...");
+
+// // Second alert after the first is closed
+// alert("Happy Coding! \n prevent this page from creating additional dialogs. ");
+
+// document.write("Welcome to jS land... Happy Coding!" ,"<br>");
+
+
+// //5. Generate the following message through browser’s developer console:
+
+// alert("Hello...i can run JS through my web browser's console");
+// console.log("Hello...i can run JS through my web browser's console");
